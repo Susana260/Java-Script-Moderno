cfc6cbff2747c4bd848fc302644ca5e3eab0ded7
Susana Borges dos Santos <susana.santoa@unidavi.edu.br>
2022-09-05 16:36:38 -0300

    Iniciei com base no trabalho "interessados"

--- index.php
+++ index.php
@@ -1,12 +1,92 @@
+<?php 
+include_once('lib/conexao.php');
+
+function lista_carros() {
+    $conn = $GLOBALS['conn'];
+    //Selecionar os interessados do Banco de Dados        
+    $sql = "SELECT Modelo,Origem,Destino,Km,Combustivel_L,Valor_Combustivel FROM laboratório order by nome";
+    $consulta = $conn->prepare($sql);
+    try {
+       $interessados = $consulta->execute();
+    }
+    catch(PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+    }   
+    echo "<table>";
+
+    $linha = "0";
+    while($r = $consulta->fetch()) {
+        echo "<tr id='l".$linha."'>";
+        echo "<td><input type='text' readonly id='n".$linha."' ondblclick='fAlterarValor(this,\"".$r["email"]."\")' value='".$r["nome"]."'></td>";
+        echo "<td><input type='text' readonly id='e".$linha."' value='".$r["email"]."'></td>";
+        echo "<td><input type='text' readonly id='f".$linha."' ondblclick='fAlterarValor(this,\"".$r["email"]."\")' value='".$r["fone"]."'></td>";
+        printf("<td><input type='button' value='D' onclick='fExcluirInteressado(\"%s\",%u)'></td>",$r["email"],$linha);
+        echo "</tr>";
+        $linha = $linha+1;
+    } 
+    echo "</table>";
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Java Script Moderno</title>
+    <link rel="stylesheet" href="lib/estilo.css">
+    <title>Interessandos</title>
 </head>
 <body>
-    
+<h1>INTERESSADOS - NewsLetter - DEVs-TI</h1>
+    <div>
+        <a href="cadastro_interessados.php">Cadastro</a>
+    </div>     
+    <br>
+    <?php
+         lista_carros();
+    ?>
 </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script language="JavaScript">
+   
+   function fAlterarValor(valor,email) 
+   {    
+      $("#"+valor.id).removeAttr("readonly"); //retira o somente leitura
+      $("#"+valor.id).attr("class","altera"); //muda o estilo
+      let campo = valor.id.slice(0,1); //seleciona a primeira letra do ID
+      
+      //criar um evento para quando for alterado um valor
+      $("#"+valor.id).change( function() {
+        let acao = "altera_valor.php?email="+email+"&valor="+$("#"+valor.id).val()+"&campo="+campo;
+        //console.log(acao);
+        //Ajax Alteração dado
+        $.get(acao, function(dados, status) {
+        //alert(dados);
+           if (status == "success") {
+               $("#"+valor.id).attr("readonly");
+               $("#"+valor.id).removeAttr("class");
+          }
+        });  
+      });
+
+      //Au sair do Input retorna as propriedades
+      $("#"+valor.id).blur( function() { 
+            $("#"+valor.id).attr("readonly");
+            $("#"+valor.id).removeAttr("class");
+        });
+
+   }
+
+   function fExcluirInteressado(email,l) {
+    let acao = "excluir_interessado.php?email="+email;
+    //Ajax para excluir o Interessado
+    $.get(acao, function(dados, status){
+        if (status == "success") {
+            $("#l"+l).remove();
+        }
+    });  
+
+   }
+</script>   
 </html>
